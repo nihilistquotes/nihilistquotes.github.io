eb859a5a0566474098e74760b3306ae758489bf5
surajk95 <sssurajsuraj@gmail.com>
2019-06-20 01:55:46 +0530

    Updates

---
+++ precache-manifest.4a665f249b8ab2b0aed96b93cb124179.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae067dd24984cbe3dc297f241a9c316",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39154d6fcd504e5369f0",
+    "url": "/static/css/main.c735b6d4.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "39154d6fcd504e5369f0",
+    "url": "/static/js/main.8dfb8692.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
