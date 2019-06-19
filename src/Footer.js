94a6828afe9eecb408422b17afa0bc18f2009dbc
surajk95 <sssurajsuraj@gmail.com>
2019-06-19 16:21:22 +0530

    added footer

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      Quotes<br/>by<br/><span class="Title">Su</span>
+    </div>
+  );
+}
+export default Footer;
---
+++ src/Quotes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Quotes = () => {
+  return (
+    <div className="Quotes">
+      Quotes
+    </div>
+  );
+}
+export default Quotes;
